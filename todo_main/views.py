bca4042dcad16d5c406f875b0b41b557ebae8a5f
jatin504 <jt872678@gmail.com>
2025-08-21 23:04:16 +0530

    Render Completed Task List On Ui

--- todo_main/views.py
+++ todo_main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from todo.models import Task
 def home(request):
     task_list = Task.objects.filter(is_completed=False).order_by('-updated_at')
+
+    completed_task_list = Task.objects.filter(is_completed=True)
     context = {
-        "tasks": task_list
+        "tasks": task_list,
+        "completed_task": completed_task_list
     }
     return render(request, 'home.html', context)
