ec3dc15536b8698c6761235d2143bc25070d5d29
jatin504 <jt872678@gmail.com>
2025-08-21 22:56:54 +0530

    Implement Add Task Functionality

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, redirect
+from todo.models import Task
 # Create your views here.
+
+def addTask(req):
+    task = req.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from todo.models import Task
 def home(request):
-    task_list = Task.objects.filter(is_completed=False)
+    task_list = Task.objects.filter(is_completed=False).order_by('-updated_at')
     context = {
         "tasks": task_list
     }
