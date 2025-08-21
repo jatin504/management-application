21b8a9c40fe83d1cf14a36b6547cdd00039a2387
jatin504 <jt872678@gmail.com>
2025-08-21 23:12:09 +0530

    Implement mark as done feature

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/', views.addTask, name='addTask')
+    path('addTask/', views.addTask, name='addTask'),
+    path('mark-as-done/<int:pk>/', views.mark_as_done, name='mark_as_done')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from todo.models import Task
 # Create your views here.
 
 def addTask(req):
     task = req.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
+
+def mark_as_done(req, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
     return redirect('home')
