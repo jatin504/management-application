7d92f1b116f6e85e487679d18fb1e6571608e7e6
jatin504 <jt872678@gmail.com>
2025-08-24 18:14:12 +0530

    Implement edit and delete task feature

--- todo/views.py
+++ todo/views.py
@@ -19,3 +19,22 @@ def mark_as_un_done(req, pk):
     task.is_completed = False
     task.save()
     return redirect('home')
+
+def edit_task(req, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if req.method == 'POST':
+        new_task = req.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            "get_task": get_task
+        }
+        return render(req, 'edit.html', context)
+
+def delete_task(req, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    get_task.delete()
+
+    return redirect('home')
